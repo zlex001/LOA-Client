44b86cf1a8418f60cb8ace7bcb56dbcd609eea8c
ZhangXi <ZhangXi@061c4e79-3c0a-3841-8ff9-c2fe60921b5a>
2026-02-18 03:40:22 +0000

    [ä¿®å¤] å®¡è®¡ä¿®å¤ï¼è§åæä»¶è¡¥å¨Unityåä¾èµãææ¡£å é¤Basicå±è¯¯åGate.csãDangerColorHelperéå½åä¸ºDangerColorUtils

---
+++ Assets/Game/Scripts/Utils/DangerColorUtils.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+namespace Game
+{
+    public static class DangerColorUtils
+    {
+        public static Color GetDangerColor(int current, int max)
+        {
+            if (max <= 0) return Color.white;
+            
+            float ratio = (float)current / max;
+            
+            if (ratio <= 0.2f)
+            {
+                return ColorUtility.TryParseHtmlString("#D43B2AFF", out Color color) ? color : Color.red;
+            }
+            else if (ratio <= 0.5f) 
+            {
+                return ColorUtility.TryParseHtmlString("#E69500FF", out Color color) ? color : new Color(1f, 0.65f, 0f, 1f);
+            }
+            else if (ratio <= 0.8f)
+            {
+                return ColorUtility.TryParseHtmlString("#6AA2E8FF", out Color color) ? color : new Color(0f, 0.5f, 1f, 1f);
+            }
+            else
+            {
+                return ColorUtility.TryParseHtmlString("#70CC70FF", out Color color) ? color : Color.green;
+            }
+        }
+        
+        public static Color GetDangerColor(float ratio)
+        {
+            if (ratio <= 0.2f)
+            {
+                return ColorUtility.TryParseHtmlString("#D43B2AFF", out Color color) ? color : Color.red;
+            }
+            else if (ratio <= 0.5f) 
+            {
+                return ColorUtility.TryParseHtmlString("#E69500FF", out Color color) ? color : new Color(1f, 0.65f, 0f, 1f);
+            }
+            else if (ratio <= 0.8f)
+            {
+                return ColorUtility.TryParseHtmlString("#6AA2E8FF", out Color color) ? color : new Color(0f, 0.5f, 1f, 1f);
+            }
+            else
+            {
+                return ColorUtility.TryParseHtmlString("#70CC70FF", out Color color) ? color : Color.green;
+            }
+        }
+    }
+}
